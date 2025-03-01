95d65ad9be0bab526e6a995b2eefc6b8aa1ca85c
Govind244 <govindsaraswat244@gmail.com>
2025-03-01 22:32:27 +0530

    query param added

---
+++ myFirstProject/src/main/java/com/FirstProject/myFirstProject/HelloController.java
@@ -0,0 +1,29 @@
+package com.FirstProject.myFirstProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloController.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Spring Boot!";
+    }
+
+    // UC 2: GET Method - Pass name as Query Parameter
+    @GetMapping("/hello/query")
+    public String sayHelloWithName(@RequestParam(name = "name", defaultValue = "Guest") String name) {
+        return "Hello, " + name + "!";
+    }
+}
